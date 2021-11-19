a0ad1212872521e9b94752404dc47d4c67ae2495
Vitalii Godlevskyi <vgodlevskyi@gmail.com>
2021-11-19 17:09:44 +0200

    fixed

--- js/task3.js
+++ js/task3.js
@@ -4,10 +4,27 @@ Develop a program “Object Projection”. Input: any JSON object; prototype obj
 Projected object structure shall be intersection of source object and prototype object structures.
 Values of properties in projected object shall be the same as values of respective properties in source object.
  */
+//Version1
+// function objectProjection(srcObj, protoObj, resObj = {}) {
+//
+//     for (let key in srcObj) {
+//         if (protoObj !== null) {
+//             let srcKey = srcObj[key];
+//             let protoKey = protoObj[key];
+//             if (protoKey === null || protoKey !== undefined) {
+//                 if (typeof srcKey === 'object') resObj[key] = {...srcKey}; else resObj[key] = srcKey;
+//             } else delete resObj[key];
+//             let resKey = resObj[key];
+//
+//             if (typeof resKey === 'object') objectProjection(srcKey, protoKey, resKey);
+//         }
+//     }
+//     return resObj;
+// }
 
 const src = {
     prop11: {
-        prop21: 21,
+        prop21: 211,
         prop22: {
             prop31: {},
             prop32: 32
@@ -49,25 +66,39 @@ const proto4 = {
 
 }
 
-const objectProjection = (src, proto, buffer = {}, result = {}) => {
+const objectProjection = (src, proto, buffer = {}, bufferKey=null, fullBuffer = {}, result = {}) => {
     for (const key in proto) {
+        //Checking if srcObject exist
         if (!src[key]) continue;
 
+        //Checking and rewrite  bufferKey
+        if (bufferKey===null) bufferKey=key;
+        fullBuffer[bufferKey]={}
+
+        //Cleaning buffer - delete all key, which are not in proto
         for (const keyB in buffer) {
             if (keyB !== key) delete buffer[keyB]
         }
 
-        buffer[key] = (typeof src[key] === 'object' ) ? {...src[key]} : src[key];
+        // filling buffer[key] on src
+        if (typeof src[key] === 'object'){
+            buffer[key]={...src[key]}
+        }else buffer=src[key]
 
+        //checking if final situation - filling result
         if (((typeof proto[key] !== 'object') || proto[key] === null)) {
+            console.log("fullBuffer--->", fullBuffer, "buffer--->", buffer);
+            fullBuffer[bufferKey]=buffer;
             result = {
                 ...result,
-                ...buffer
+                ...fullBuffer
             }
             buffer = {}
         }
+        console.log("result---->", result)
 
-        if (typeof buffer[key] === 'object') objectProjection(src[key], proto[key], buffer[key], result);
+        if (typeof buffer[key] === 'object') objectProjection(src[key], proto[key], buffer[key], bufferKey, fullBuffer, result);
+        console.log("result with rec---->", result)
     }
     return result;
 }
@@ -89,9 +120,7 @@ console.log("Expected result of objectProjection case3 -  { prop12: 12 }\n");
 
 console.log("Proto object case4 - ", proto4);
 console.log("Result of function objectProjection case4 - ", objectProjection(src, proto4));
-console.log("Expected result of objectProjection case4 -  { }\n");
-
-
+console.log("Expected result of objectProjection case4 -  {}\n");
 
 
 // function objectCleaner(obj) {
