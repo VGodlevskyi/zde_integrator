4a10851f6c6d41131face87ab5ca68ad53831757
Vitalii Godlevskyi <vgodlevskyi@gmail.com>
2021-11-19 13:44:40 +0200

    fixed

--- js/task3.js
+++ js/task3.js
@@ -4,19 +4,12 @@ Develop a program “Object Projection”. Input: any JSON object; prototype obj
 Projected object structure shall be intersection of source object and prototype object structures.
 Values of properties in projected object shall be the same as values of respective properties in source object.
  */
-// function objectCleaner(obj) {
-//     Object.keys(obj).forEach(key => {
-//         if (obj[key] && typeof obj[key] === 'object' && objectCleaner(obj[key]) === null) delete obj[key];
-//     });
-//     return Object.keys(obj).length ? obj : null;
-// }
-//1 var
 
 const src = {
     prop11: {
         prop21: 21,
         prop22: {
-            prop31: 12,
+            prop31: {},
             prop32: 32
         }
     },
@@ -39,7 +32,7 @@ const proto1 = {
 const proto2 = {
     prop11: {
         prop22: {
-            prop31: {}
+            prop31: null
         }
     }
 }
@@ -49,7 +42,6 @@ const proto3 = {
     prop11: {
         prop23: null
     },
-
 }
 
 const proto4 = {
@@ -59,22 +51,24 @@ const proto4 = {
 
 const objectProjection = (src, proto, buffer = {}, result = {}) => {
     for (const key in proto) {
-        console.log("result begin--------", result)
         if (!src[key]) continue;
-        buffer[key] = typeof src[key] === 'object' ? {...src[key]} : src[key];
 
-        if (((typeof proto[key] !== 'object') || proto[key]===null)) {
-            console.log("условие выхода")
+        for (const keyB in buffer) {
+            if (keyB !== key) delete buffer[keyB]
+        }
+
+        buffer[key] = (typeof src[key] === 'object' ) ? {...src[key]} : src[key];
+
+        if (((typeof proto[key] !== 'object') || proto[key] === null)) {
             result = {
                 ...result,
                 ...buffer
             }
             buffer = {}
         }
-        console.log("buffer", buffer, "result", result)
+
         if (typeof buffer[key] === 'object') objectProjection(src[key], proto[key], buffer[key], result);
     }
-    console.log("result end--------", result)
     return result;
 }
 
@@ -96,3 +90,13 @@ console.log("Expected result of objectProjection case3 -  { prop12: 12 }\n");
 console.log("Proto object case4 - ", proto4);
 console.log("Result of function objectProjection case4 - ", objectProjection(src, proto4));
 console.log("Expected result of objectProjection case4 -  { }\n");
+
+
+
+
+// function objectCleaner(obj) {
+//     Object.keys(obj).forEach(key => {
+//         if (obj[key] && typeof obj[key] === 'object' && objectCleaner(obj[key]) === null) delete obj[key];
+//     });
+//     return Object.keys(obj).length ? obj : null;
+// }
