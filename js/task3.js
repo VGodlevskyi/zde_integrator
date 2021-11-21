d72f32135bd192438cdaebef8648793ea9fdbea9
Vitalii Godlevskyi <vgodlevskyi@gmail.com>
2021-11-21 21:20:55 +0200

    upload

--- js/task3.js
+++ js/task3.js
@@ -5,25 +5,65 @@ Projected object structure shall be intersection of source object and prototype 
 Values of properties in projected object shall be the same as values of respective properties in source object.
  */
 
-//Version1
-// function objectProjection(srcObj, protoObj, resObj = {srcObj}, final = {}) {
+//V1
+// function objectProjection(srcObj, protoObj, resObj = {}) {
 //
 //     for (let key in protoObj) {
-//         if (srcObj[key]) {
-//             let srcKey = srcObj[key];
-//             let protoKey = protoObj[key];
-//             if (typeof srcKey === 'object') resObj[key] = {...protoKey}; else resObj[key] = srcKey;
-//             final = {
-//                 ...final,
-//                 ...resObj
-//             }
+//         if (!srcObj[key]) continue;
+//         let srcKey = srcObj[key];
+//         let protoKey = protoObj[key];
 //
-//             let resKey = resObj[key];
-//
-//             if (typeof resKey === 'object') objectProjection(srcKey, protoKey, resKey, final);
+//         if (protoKey === null) {
+//             if (typeof srcKey === 'object') resObj[key] = {...srcKey}; else resObj[key] = srcKey;
+//         continue;
 //         }
+//         if (typeof protoKey !== 'object') {
+//             if (typeof srcKey === 'object') resObj[key] = {...srcKey}; else resObj[key] = srcKey;
+//         continue;
+//         }
+//
+//
+//         let resKey = resObj[key];
+//
+//         if (typeof srcKey === 'object') objectProjection(srcKey, protoKey, resKey);
+//
 //     }
-//     return final;
+//     return resObj;
+// }
+
+// V3
+// const objectProjection = (src, proto, buffer = {src}, bufferKey = null, fullBuffer = {}, result = {}) => {
+//     for (const key in proto) {
+//         //Checking if srcObject exist
+//         if (!src[key]) continue;
+//
+//         //Filling fullBuffer
+//         bufferKey === null ? fullBuffer[key] = {} : fullBuffer[bufferKey] = {}
+//
+//         //Cleaning buffer - delete all key, which are not in proto
+//         for (const keyB in buffer) {
+//             if (keyB !== key) delete buffer[keyB]
+//         }
+//
+//         // filling buffer[key] on src
+//         if (typeof src[key] === 'object') {
+//             buffer[key] = {...src[key]}
+//         } else {
+//             buffer[key] = src[key]
+//         }
+//
+//         //checking if final situation - filling result
+//         if (((typeof proto[key] !== 'object') || proto[key] === null)) {
+//             bufferKey === null ? fullBuffer[key] = buffer[key] : fullBuffer[bufferKey] = buffer;
+//             result = {
+//                 ...result,
+//                 ...fullBuffer
+//             }
+//             buffer = {}
+//         }
+//         if (typeof buffer[key] === 'object') objectProjection(src[key], proto[key], buffer[key], bufferKey, fullBuffer, result);
+//     }
+//     return result;
 // }
 
 const src = {
@@ -54,12 +94,11 @@ const proto1 = {
 
 const proto2 = {
     prop11: {
-        prop21: 211,
+        prop21: 213,
         prop22: {
             prop31: 32
         }
-    },
-    prop12:0
+    }
 }
 
 const proto3 = {
@@ -73,12 +112,12 @@ const proto4 = {
     prop12: {prop15: {prod14: 12}},
 
 }
-
+//v4
 const objectProjection = (src, proto, result = {}) => {
     let buffer = {}
 
     for (protoKey in proto) {
-        if (src.hasOwnProperty(protoKey)) buffer[protoKey] = src[protoKey]
+        if (src.hasOwnProperty(protoKey)) buffer[protoKey] = src[protoKey];
     }
     console.log("buffer---", buffer)
     for (const key in proto) {
@@ -96,19 +135,19 @@ const objectProjection = (src, proto, result = {}) => {
                 ...result,
                 [key]: buffer[key]
             }
-            console.log("result2---", result, "proto[key]---", proto[key], "buffk", buffer[key])
         }
         if (typeof proto[key] === 'object' && typeof proto[key] !== null) objectProjection(src[key], proto[key], result);
     }
     return result;
 }
 
-// console.log("Task3");
-// console.log("-----------------------------------------------------------------------------------");
-// console.log("Src object - ", src);
-// console.log("Proto object case1 - ", proto1);
-// console.log("Result of function objectProjection case1 - ", objectProjection(src, proto1));
-// console.log("Expected result of objectProjection case1 -  { prop11: { prop22: { prop31: {}, prop32: 32 } },  prop12: 12 } }\n")
+
+console.log("Task3");
+console.log("-----------------------------------------------------------------------------------");
+console.log("Src object - ", src);
+console.log("Proto object case1 - ", proto1);
+console.log("Result of function objectProjection case1 - ", objectProjection(src, proto1));
+console.log("Expected result of objectProjection case1 -  { prop11: { prop22: { prop31: {}, prop32: 32 } },  prop12: 12 } }\n")
 
 console.log("Proto object case2 - ", proto2);
 console.log("Result of function objectProjection case2 - ", objectProjection(src, proto2));
