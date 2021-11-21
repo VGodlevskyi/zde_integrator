a9a4cec77efa980ceb8637b91b137121275ec383
Vitalii Godlevskyi <vgodlevskyi@gmail.com>
2021-11-22 00:21:29 +0200

    upload

--- js/task3.js
+++ js/task3.js
@@ -5,6 +5,91 @@ Projected object structure shall be intersection of source object and prototype 
 Values of properties in projected object shall be the same as values of respective properties in source object.
  */
 
+const objectProjection = (src, proto, buffer = {}, result = {}) => {
+
+    for (const key in proto) {
+        if (!src[key]) continue;
+        buffer[key] = {}
+        if (proto[key] === null || typeof proto[key] !== 'object') {
+
+            buffer[key] = typeof src[key] === 'object' ? {...src[key]} : src[key];
+            result = {
+                ...result,
+                ...buffer
+            }
+            return result;
+        }
+        if (typeof proto[key] === 'object' && typeof proto[key] !== null)
+            objectProjection(src[key], proto[key], buffer[key], result);
+    }
+    return result;
+}
+
+const src = {
+    prop11: {
+        prop21: 211,
+        prop22: {
+            prop31: {},
+            prop32: 32
+        }
+    },
+    prop12: 12
+}
+
+const proto1 = {
+    prop11: {
+        prop22: 1
+    },
+    prop12: 123
+
+}
+
+const proto2 = {
+    prop11: {
+        prop21: null
+    },
+    prop12: null
+}
+
+const proto3 = {
+    prop11: {
+        prop21: 211,
+        prop22: {
+            prop31: {
+                prop41: 2
+            }
+        },
+    },
+    prop12: 32
+}
+
+const proto4 = {
+    prop12: {
+        prop15: null
+    }
+}
+
+console.log("Task3");
+console.log("-----------------------------------------------------------------------------------");
+console.log("Src object - ", src);
+console.log("Proto object case1 - ", proto1);
+console.log("Result of function objectProjection case1 - ", objectProjection(src, proto1));
+console.log("Expected result of objectProjection case1 -  { prop11: { prop22: { prop31: {}, prop32: 32 } }, prop12: 12 } }\n")
+
+console.log("Proto object case2 - ", proto2);
+console.log("Result of function objectProjection case2 - ", objectProjection(src, proto2));
+console.log("Expected result of objectProjection case2 -  { prop11: { prop21: 211 }, prop12: 12 }\n");
+
+console.log("Proto object case3 - ", proto3);
+console.log("Result of function objectProjection case3 - ", objectProjection(src, proto3));
+console.log("Expected result of objectProjection case3 -  { prop11: { prop21: 211 }, prop12: 12 }\n");
+
+console.log("Proto object case4 - ", proto4);
+console.log("Result of function objectProjection case4 - ", objectProjection(src, proto4));
+console.log("Expected result of objectProjection case4 -  {}\n");
+
+
+
 //V1
 // function objectProjection(srcObj, protoObj, resObj = {}) {
 //
@@ -21,17 +106,14 @@ Values of properties in projected object shall be the same as values of respecti
 //             if (typeof srcKey === 'object') resObj[key] = {...srcKey}; else resObj[key] = srcKey;
 //         continue;
 //         }
-//
-//
 //         let resKey = resObj[key];
 //
 //         if (typeof srcKey === 'object') objectProjection(srcKey, protoKey, resKey);
-//
 //     }
 //     return resObj;
 // }
 
-// V3
+// V2
 // const objectProjection = (src, proto, buffer = {src}, bufferKey = null, fullBuffer = {}, result = {}) => {
 //     for (const key in proto) {
 //         //Checking if srcObject exist
@@ -65,101 +147,3 @@ Values of properties in projected object shall be the same as values of respecti
 //     }
 //     return result;
 // }
-
-const src = {
-    prop11: {
-        prop21: 211,
-        prop22: {
-            prop31: {},
-            prop32: 32
-        }
-    },
-    prop12: 12,
-    prop30: {
-        prop31: {
-            prop311: 22,
-            prop312: 32
-        },
-        prop15: 23
-    }
-}
-
-const proto1 = {
-    prop11: {
-        prop22: null
-    },
-    prop12: null
-
-}
-
-const proto2 = {
-    prop11: {
-        prop21: 213,
-        prop22: {
-            prop31: 32
-        }
-    }
-}
-
-const proto3 = {
-    prop12: 32,
-    prop11: {
-        prop23: null
-    },
-}
-
-const proto4 = {
-    prop12: {prop15: {prod14: 12}},
-
-}
-//v4
-const objectProjection = (src, proto, buffer={},result = {}) => {
-
-    // for (protoKey in proto) {
-    //     if (src.hasOwnProperty(protoKey)) {
-    //         buffer[protoKey] = src[protoKey];}
-    // }
-
-
-    console.log("buffer---", buffer)
-    for (const key in proto) {
-        if (!src[key]) continue;
-        buffer[key]={}
-        if (proto[key] === null) {
-            buffer[key] = typeof src[key] === 'object' ? {...src[key]} : src[key];
-            result = {
-                ...result,
-                ...buffer
-            }
-            console.log("result---", result)
-        } else if (typeof proto[key] !== 'object'  ) {
-
-            result = {
-                ...result,
-                [key]: src[key]
-            }
-        }
-        if (typeof proto[key] === 'object' && typeof proto[key] !== null)
-            objectProjection(src[key], proto[key], buffer[key],result);
-    }
-    return result;
-}
-
-console.log("Task3");
-console.log("-----------------------------------------------------------------------------------");
-console.log("Src object - ", src);
-console.log("Proto object case1 - ", proto1);
-console.log("Result of function objectProjection case1 - ", objectProjection(src, proto1));
-console.log("Expected result of objectProjection case1 -  { prop11: { prop22: { prop31: {}, prop32: 32 } },  prop12: 12 } }\n")
-
-console.log("Proto object case2 - ", proto2);
-console.log("Result of function objectProjection case2 - ", objectProjection(src, proto2));
-console.log("Expected result of objectProjection case2 -  { prop11: { prop22: { prop31: {}}}\n");
-
-console.log("Proto object case3 - ", proto3);
-console.log("Result of function objectProjection case3 - ", objectProjection(src, proto3));
-console.log("Expected result of objectProjection case3 -  { prop12: 12 }\n");
-
-console.log("Proto object case4 - ", proto4);
-console.log("Result of function objectProjection case4 - ", objectProjection(src, proto4));
-console.log("Expected result of objectProjection case4 -  {}\n");
